77aa7f2b253744a1cfa4aa1df3dd6f580691443d
IanDBrown <ian@robertwbrown.net>
2023-11-12 22:49:42 -0500

    Added previous tide and increased the tide call to three days instead of just two

--- src/FrontPageHolder.js
+++ src/FrontPageHolder.js
@@ -5,10 +5,10 @@ const FrontPageHolder = (props) => {
     const [loaded, setLoaded] = useState(false)
     const date = new Date();
     let offset = -300;
-    let todaysDate =  new Date(date.getTime() + offset*60*1000).toJSON().slice(0,10).replace(/-/g,'')
+    let todaysDate =  new Date((date.getTime() -86400000) + offset*60*1000).toJSON().slice(0,10).replace(/-/g,'')
     useEffect(()=>{
         const fetchTideData = async () =>{
-            await fetch(`https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=${todaysDate}&range=48&station=8551910&product=predictions&datum=MLLW&time_zone=lst_ldt&interval=hilo&units=english&application=DataAPI_Sample&format=json`)
+            await fetch(`https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=${todaysDate}&range=72&station=8551910&product=predictions&datum=MLLW&time_zone=lst_ldt&interval=hilo&units=english&application=DataAPI_Sample&format=json`)
             .then((res)=> res.json())
             .then((data)=> setTideData(data))
             setLoaded(true)
@@ -18,7 +18,8 @@ const FrontPageHolder = (props) => {
 
     let highTide = [];
     let lowTide = [];
-    let closestTide = [];
+    let upComingTide = [];
+    let previousTide = []
     if(loaded){
         function spliceTime(givenTime){
             let date = givenTime.slice(5, 10)
@@ -36,6 +37,7 @@ const FrontPageHolder = (props) => {
             else if (hour > 12) {
                 hour = hour - 12
                 if (hour < 10) hour = " 0" + hour
+                else hour = " " + hour
             }
             return date + hour + minute + AmOrPm
         }
@@ -54,35 +56,45 @@ const FrontPageHolder = (props) => {
             return now_utc
         }
         
-        function findUpcomingTide(givenTime, tideType, height){
+        function findUpcomingTide(givenTime, tideType, height, index){
             let currDate = UtcDate(datetime)
             let givenDate = UtcDate(givenTime)
-            console.log(currDate, givenDate)
-            if(currDate < givenDate && closestTide.length === 0){
-                [closestTide[0], closestTide[1], closestTide[3]] = [miltaryToTwelve(givenTime), height, tideType]
-            }
+            if(currDate < givenDate && upComingTide.length === 0){
+                [upComingTide[0], upComingTide[1], upComingTide[2]] = [miltaryToTwelve(givenTime), height, tideType]
+                // [previousTide[0], previousTide[1], previousTide[2]] = [miltaryToTwelve(tideData.predictions[index-1].t), tideData.predictions[index-1].v, tideData.predictions[index-1].type]
+                previousTide[0] = miltaryToTwelve(tideData.predictions[index-1].t)
+                previousTide[1] = tideData.predictions[index-1].v
+                previousTide[2] = tideData.predictions[index-1].type
+            } 
         }
 
-        tideData.predictions.map((extreme) => {
+        tideData.predictions.map((extreme, n) => {
             let tideObj = {}
-            findUpcomingTide(extreme.t, extreme.type, extreme.v)
+            findUpcomingTide(extreme.t, extreme.type, extreme.v, n)
             tideObj["height"] = extreme.v
             tideObj["date"] = miltaryToTwelve(extreme.t)
             extreme.type === "L" ? lowTide.push(tideObj) : highTide.push(tideObj)
         })
+        console.log(highTide)
     }
     if(!loaded){ return <p>Loading...</p>}
     else return (
         <div className="container">
-            <div className="currentBox">
-                <h4 className="tide-title">Upcoming Tide:</h4>
-                <h1>{closestTide[2] === "H" ? "High Tide" : "Low Tide"}</h1>
-                <h3>{closestTide[0]}</h3>
-                <h2>{closestTide[1]} ft.</h2>
-                
+            <div className="prev-next-tide">
+                <div className="currentBox previous-tide">
+                    <h4 className="tide-title">Previous Tide:</h4>
+                    <h1>{previousTide[2] === "H" ? "High Tide" : "Low Tide"}</h1>
+                    <h3>{previousTide[0]}</h3>
+                    <h2>{previousTide[1]} ft.</h2>
+                </div>
+                <div className="currentBox upcoming-tide">
+                    <h4 className="tide-title">Upcoming Tide:</h4>
+                    <h1>{upComingTide[2] === "H" ? "High Tide" : "Low Tide"}</h1>
+                    <h3>{upComingTide[0]}</h3>
+                    <h2>{upComingTide[1]} ft.</h2>
+                </div>
             </div>
             <div className="extreme-box">
-
                 <div className="extremes-high extremes">
                     <h4 className="tide-title">High Tide:</h4>
                     <div className="tide-data">
