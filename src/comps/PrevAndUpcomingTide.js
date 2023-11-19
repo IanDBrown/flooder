d785055484dc26852665fe9f483911c2f940b409
IanDBrown <ian@robertwbrown.net>
2023-11-18 22:12:38 -0500

    Worked on the font/boldness of the layout

--- src/comps/PrevAndUpcomingTide.js
+++ src/comps/PrevAndUpcomingTide.js
@@ -3,7 +3,7 @@ const PrevAndUpcomingTide = (props) => {
         <div className="currentBox">
             <h4 className="tide-title">{props.typeOfTide}<br></br>Tide</h4>
             <h1>{props.tide[2] === "H" ? "High Tide" : "Low Tide"}</h1>
-            <h3><b>{props.tide[1]} ft.</b> at</h3>
+            <h3><b>{props.tide[1]}</b> ft. at</h3>
             <h3>{props.tide[0]}</h3>
         </div>
      );
